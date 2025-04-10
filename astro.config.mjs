9cbb9d27cae9544adb1654b60942ec47ccb6fc08
Alod Ruiz Luna <aldo.ruiz.luna@gmail.com>
2025-04-10 01:57:58 -0600

    fix: Resolve animejs import error and fix Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [tailwind()],
   output: 'server',
   adapter: node({
-    mode: 'standalone',
+    mode: 'standalone'
   }),
   server: {
-    port: 3000,
-  },
+    port: 4321 // Use default Astro port
+  }
 });
