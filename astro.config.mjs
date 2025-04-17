1495e564f49ab9a199875c1a36ad53e723e54455
Alod Ruiz Luna <aldo.ruiz.luna@gmail.com>
2025-04-17 00:13:13 -0600

    Prepare codebase for production deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,22 @@
 import { defineConfig } from 'astro/config';
+import 'dotenv/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 
 // Determine base path based on the command being run
-const base = process.env.npm_lifecycle_event === 'build'
-  ? '/catalogo100ecomm' // Use repo name for GitHub Pages build
-  : '/'; // Use root for local development
+const defaultBase = process.env.npm_lifecycle_event === 'build'
+  ? '/catalogo100ecomm'
+  : '/';
+const base = process.env.BASE_PATH || defaultBase;
+const siteUrl = process.env.SITE_URL || 'https://aldoruizluna.github.io';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: node(),
   integrations: [tailwind()],
   output: 'server',  
-  site: 'https://aldoruizluna.github.io', // Replace with your GitHub username
-  base: base, // Use the conditional base path
+  site: siteUrl,
+  base: base,
   build: {
     // If you have assets that need special handling
     assets: '_assets'
